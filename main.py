4b51b4d46a140e3f3e3e9f83c12b5863465655f1
AhmedTechno93 <Ahmedtechno93@gmail.com>
2021-10-10 21:36:40 +0300

    add Function to find the class of input ip

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 class Solution:
-    pass
+ def Input_ip():
+        ip = input('Enter your ip address: ')
+        return ip
+    
+
+ def findClass(ip):
+    if(ip[0] >= 0 and ip[0] <= 127):
+        return "A"
+    elif(ip[0] >=128 and ip[0] <= 191):
+        return "B"
+    elif(ip[0] >= 192 and ip[0] <= 223):
+        return "C"
+    elif(ip[0] >= 224 and ip[0] <= 239):
+        return "D"
+    else:
+        return "E"
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    ip = input('Enter your ip address: ')
+    ip = ip.split(".")
+    ip = [int(i) for i in ip]
+
+#find class
+Class = Solution.findClass(ip)
+print(" Class: ",Class,",")
