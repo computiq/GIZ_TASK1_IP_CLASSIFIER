4f3a6e15f43d68e3935a6625b9faee3d9eeecc81
fatima <fatimaalaa2001xx@gmail.com>
2021-10-12 09:47:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-class Solution:
-    pass
+
+#first we must split
+ip=input("please enter an IP address:")
+spliting=ip.split('.')
+a='.'
+address=int(spliting[0])
+#classes
+ClassApri=range(10)
+spi=range(127)
+classBpri=range(172)
+classCpri=range(192)
+ClassA= range(0,127)
+ClassB= range(128,191)
+ClassC= range(192,223)
+ClassD= range(224,239)
+ClassE= range(240,255)
+
+if address in ClassApri:
+    print("Class A Designation private")
+elif address in spi:
+    print("Class A  Designation special")
+elif address in ClassA:
+    print("Class A Designation public")
+elif address in classBpri:
+    print("Class B Designation private")
+elif address in ClassB:
+    print("Class B public")
+elif address in classCpri:
+    print("Class C Designation private")
+elif address in ClassC:
+    print("Class C Designation public")
+elif address in ClassD:
+    print("Class D Designation public")
+elif address in ClassE:
+    print("Class E Designation public")
 
 
-if __name__ == '__main__':
-    pass
+
+
