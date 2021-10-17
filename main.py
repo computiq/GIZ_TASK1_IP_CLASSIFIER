ff6965c3d41874efe8bafcd2da213c3bb1e4069d
badour <badour.alsamaraie@gmail.com>
2021-10-18 02:26:07 +0300

    task_1 finish

--- main.py
+++ main.py
@@ -1,55 +1,76 @@
-import sys
+import re
+
 class Solution:
+    pass
       
     
-    def parse_ip(self, ip):
-        classname= ''
-        designation =''
-        if(ip[0] >= 0 and ip[0] <= 127):
-          classname= "A"
-          if (ip[1] >= 0 and ip[1] <= 255 and ip[2] >=0 and ip[2] <=255 and ip[3] >=0 and ip[3] <=255):
+    def parse_ip(self):
+          classname= ''
+          designation =''
+
+          while True:
+               
+               ip = input('input ip addess : ')
+               
+               
+               if re.match('^(([01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}([01]?\d{1,2}|2[0-4]\d|25[0-5])(/([12]?[0-9]|3[01]))$',ip):
+                   break
+               else:
+
+                   print('inset other format')
+          
+          
+          firstoct, *remainoct =  list(map(int, ip[:ip.index("/")].split('.')))
+
+          if(firstoct >= 0 and firstoct <= 127):
+               classname= "A"
+               if firstoct ==10 and all(0 <= i <= 255 for i in remainoct):
+                    designation='private'
+                 
+               else:
+                    designation='special'
+          
+
+          elif(firstoct >=128 and firstoct <= 191):
+               classname= "B"
+               #designation='public'
+               secondoct = remainoct[0]
+               if firstoct==172 and (secondoct >= 16 and secondoct <= 31) and all(0 <= i <= 255 for i in remainoct[1:3]):
+                    designation='private'
+               else:
+                    designation='public'
+          
+          elif(firstoct >= 192 and firstoct <= 223):
+               classname= "C"
+               secondoct = remainoct[0]
+               if firstoct == 192 and secondoct== 168 and all(0 <= i <= 255 for i in remainoct[1:3]):
+                    designation='private'
+               else:
+                    designation='public'
+          
+          elif(firstoct >= 224 and firstoct <= 239):
+               classname= "D"
                designation='special'
+          
           else:
-               designation='private'
-        
-   
-        elif(ip[0] >=128 and ip[0] <= 191):
-             classname= "B"
-             if (ip[1] >= 16 and ip[1] <= 31):
-               designation='private'
-        
-        elif(ip[0] >= 192 and ip[0] <= 223):
-             classname= "C"
-             if (ip[1] >= 168 and ip[1] <= 255):
-               designation='private'
-        
-        elif(ip[0] >= 224 and ip[0] <= 239):
-             classname= "D"
-             if (ip[1] >= 0 and ip[1] <= 255):
-               designation='private'
-        
-        else:
-             classname= "special"
-        return classname, designation
-        
-    
+               print("not valid ip")
+
+          print("ip class is  "+ classname + " and desifnation :" + designation)
+          
+
 
 
 
 
 if __name__ == '__main__':
-    #ip = input('')
-    #ip= input.split("/")
-    #print(input)
-    ip = "192.226.12.11"
-    ip = ip.split(".")
-    sample = Solution()
+     pass
+     
+     sample= Solution()
+     sample.parse_ip()
     
-    ip = [int(i) for i in ip]
-
-    #getting the network class
-    networkcalss= sample.parse_ip(ip)
-    print("ip class is: "+ networkcalss[0], "the IP address designation : "+ networkcalss[1])
+    
+        
+    
     
    
         
